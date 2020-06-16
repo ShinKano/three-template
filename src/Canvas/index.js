18587003e668645f5b49f12e940b2ba63b2a31cb
ShinKano <yushph@gmail.com>
2020-06-16 21:22:10 +0900

    init

--- src/Canvas/index.js
+++ src/Canvas/index.js
@@ -1,20 +1,27 @@
-import * as THREE from 'three';
+import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer';
+import { OrthographicCamera } from 'three/src/cameras/OrthographicCamera';
+import { Scene } from 'three/src/scenes/Scene';
+import { PlaneGeometry } from 'three/src/geometries/PlaneGeometry';
+import { ShaderMaterial } from 'three/src/materials/ShaderMaterial';
+import { MeshBasicMaterial } from 'three/src/materials/MeshBasicMaterial';
+import { Mesh } from 'three/src/objects/Mesh';
+import { Vector2 } from 'three/src/math/Vector2';
+
 // シェーダーソース
 import vertexSource from './shaders/shader.vert';
 import fragmentSource from './shaders/shader.frag';
 
 export default class Canvas {
   constructor() {
-    // マウス座標
-    this.mouse = new THREE.Vector2(0, 0);
-    // スクロール量
-    this.scrollY = 0;
     // ウィンドウサイズ
     this.w = window.innerWidth;
     this.h = window.innerHeight;
 
+    // マウス座標
+    this.mouse = new Vector2(0.5, 0.5);
+
     // レンダラーを作成
-    this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.renderer = new WebGLRenderer();
     this.renderer.setSize(this.w, this.h);// 描画サイズ
     this.renderer.setPixelRatio(window.devicePixelRatio);// ピクセル比
 
@@ -22,98 +29,65 @@ export default class Canvas {
     const container = document.getElementById("canvas-container");
     container.appendChild(this.renderer.domElement);
 
-
-    // ウィンドウサイズの平面がぴったり収まるカメラ距離（dist）を計算
-    const fov    = 60; // 視野角
-    const fovRad = (fov / 2) * (Math.PI / 180);// 視野角をラジアンに変換
-    const dist   = (this.h / 2) / Math.tan(fovRad);// ウィンドウぴったりのカメラ距離
-
-    // カメラを作成 (視野角, 画面のアスペクト比, カメラに映る最短距離, カメラに映る最遠距離)
-    this.camera = new THREE.PerspectiveCamera(fov, this.w / this.h, 1, dist * 2);
-    this.camera.position.z = dist;// カメラを遠ざける
-
+    // カメラを作成（背景シェーダーだけならパースいらないので、OrthographicCameraをつかう）
+    this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, -1);
 
     // シーンを作成
-    this.scene = new THREE.Scene();
+    this.scene = new Scene();
 
-    // ライトを作成
-    this.light = new THREE.PointLight(0x00ffff);
-    this.light.position.set(0, 0, 400);// ライトの初期位置を設定
-
-    // ライトをシーンに追加
-    this.scene.add(this.light);
-
-    // ---------- 立方体のサンプル
-    // // 立方体のジオメトリを作成(幅, 高さ, 奥行き)
-    // const geo = new THREE.BoxGeometry(300, 300, 300);
-
-    // // マテリアルを作成
-    // const mat = new THREE.MeshLambertMaterial({ color: 0xffffff });
-
-    // // ジオメトリとマテリアルからメッシュを作成
-    // this.mesh = new THREE.Mesh(geo, mat);
-    // // メッシュを回転させる
-    // this.mesh.rotation.x = THREE.MathUtils.DEG2RAD * 25;
-    // this.mesh.rotation.y = THREE.MathUtils.DEG2RAD * 45;
-
-
-    // ---------- 自作シェーダーのサンプル
     // 平面をつくる（幅, 高さ, 横分割数, 縦分割数）
-    const geo = new THREE.PlaneGeometry(2, 2, 10, 10);
+    const geo = new PlaneGeometry(2, 2, 10, 10);
 
-    // シェーダーソースを渡してマテリアルを作成
-    const mat = new THREE.ShaderMaterial({
+
+    // uniform変数を定義
+    this.uniforms = {
+      uAspect: {
+        value: this.w / this.h
+      },
+      uTime: {
+        value: 0.0
+      },
+      uMouse: {
+        value: new Vector2(0.5, 0.5)// this.mouse とは別のベクトル
+      }
+    };
+    // uniform変数とシェーダーソースを渡してマテリアルを作成
+    const mat = new ShaderMaterial({
+      uniforms: this.uniforms,
       vertexShader: vertexSource,
-      fragmentShader: fragmentSource,
-      wireframe: true
+      fragmentShader: fragmentSource
     });
 
-    this.mesh = new THREE.Mesh(geo, mat);
+    this.mesh = new Mesh(geo, mat);
 
     // メッシュをシーンに追加
     this.scene.add(this.mesh);
 
-    // 画面に表示
-    this.renderer.render(this.scene, this.camera);
-    // アニメーション描画ループ開始
+     // 描画ループ開始
     this.render();
   }
-
 
   render() {
     // 次のフレームを要求
     requestAnimationFrame(() => { this.render(); });
+
     // ミリ秒から秒に変換
     const sec = performance.now() / 1000;
 
-    // 1秒で45°回転する
-    this.mesh.rotation.x = sec * (Math.PI / 4);
-    this.mesh.rotation.y = sec * (Math.PI / 4);
+    // シェーダーに渡す時間を更新
+    this.uniforms.uTime.value = sec;
 
-    // スクロール量より少なく動く
-    this.mesh.position.y = this.scrollY * 0.5;
+    // シェーダーに渡すマウスを更新
+    this.uniforms.uMouse.value.lerp(this.mouse, 0.05);
 
     // 画面に表示
     this.renderer.render(this.scene, this.camera);
   }
 
 
-  // ---------- その他関数 ----------
-
-  // マウス座標を取得
-  // 画面の中心が原点（0, 0）なので、計算が必要
   mouseMoved(x, y) {
-    this.mouse.x =  x - (this.w / 2);// 原点を中心に持ってくる
-    this.mouse.y = -y + (this.h / 2);// 軸を反転して原点を中心に持ってくる
-
-    // ライトの xy座標 をマウス位置にする
-    this.light.position.x = this.mouse.x;
-    this.light.position.y = this.mouse.y;
-  }
-
-  // スクロール量を取得
-  scrolled(y) {
-    this.scrollY = y;
-    console.log(this.scrollY);
+    // 左上原点から左下原点に変換
+    this.mouse.x = x / this.w;
+    this.mouse.y = 1.0 - (y / this.h);
   }
 };
--- src/Canvas/shaders/shader.vert
+++ src/Canvas/shaders/shader.vert
@@ -1,8 +1,11 @@
 // vertex shader ( 頂点シェーダー )
 // このファイルに各頂点ごとの処理を記述します
 
+// 変数
+varying vec2 vUv;
+
 void main() {
-  vec3 pos = position;// position: ShaderMaterialで補完される vec3 型(xyz)の変数。ジオメトリの頂点のこと。
-  // pos.y = ( pos.y * 0.5 ) + sin( pos.x * 3.0 ) * 0.5;// 縦を半分のサイズにして、sinでy座標を歪ませる
-  gl_Position = vec4( pos, 1.0 );
+  vUv = uv;// uv: ShaderMaterialで補完される vec2 型(xy)の変数。テクスチャ座標のこと。
+
+  gl_Position = vec4( position, 1.0 );
 }
